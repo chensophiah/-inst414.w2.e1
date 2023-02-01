e2d005f9238062e0a1298b5a538cfc4098992b69
Sophia Chen <81427254+chensophiah@users.noreply.github.com>
2023-02-01 12:13:43 -0500

    Add files via upload

---
+++ w2e1.py
@@ -0,0 +1,16 @@
+sum = 0.0
+count = 0.0
+rating = 0.0
+
+with open("imdb_movies_1985to2022.json", "r") as f:
+    for line in in f:
+        this_movie = json.loads(line)
+        actors = this_movie["actors"]
+        for a in actors:
+            if a[1] == "Hugh Jackman":
+                r = this_movie["rating"]
+                sum += r["avg"]
+                count += 1
+                
+rating = sum/count
+print(rating)
